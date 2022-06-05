e9b7293db61168299d65e3f972734dd6ac176f51
MaSander <ma.sander11@gmail.com>
2022-06-06 00:10:47 +0200

    retornando linhas, só falta resolver se não tiver mais linhas

--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msander- <msander-@student.42sp.org.br     +#+  +:+       +#+        */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 15:02:07 by msander-          #+#    #+#             */
-/*   Updated: 2022/06/02 13:51:24 by msander-         ###   ########.fr       */
+/*   Updated: 2022/06/05 23:39:11 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@
 char    *get_next_line(int fd);
 int	ft_isascii(int c);
 size_t	ft_strlen(const char *c);
-char	*ft_have_complete_line(char *buff);
+char	*ft_get_end_line(char *buff);
 char	*ft_return_line(char *buff);
+char    *ft_strdup(char *str, int size);
+int     ft_str_have_newline(char *buff);
 
 
 
--- main.c
+++ main.c
@@ -9,14 +9,18 @@ int main()
 
     fd = open("teste", O_RDONLY);
 
-    int i = 2;
+    int i = 10;
+    int line_count = 0;
+
+    printf("\n\t ____START____\n");
 
     while(i--)
     {
-        printf("linha %i:\n", i);
-        printf("\n%s\n", get_next_line(fd));
+        printf("linha %i:\n", line_count++);
+        printf("%s\n", get_next_line(fd));
     }
 
+    printf("\n\t ____DONE____");
     close(fd);
 
     return (0);
