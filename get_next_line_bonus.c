a4088f58beb7204be284929da2fb944899c4c049
MaSander <ma.sander11@gmail.com>
2022-06-17 18:49:08 +0200

    include bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/05 11:06:50 by msander-          #+#    #+#             */
+/*   Updated: 2022/06/17 18:48:36 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_get_line(char *str)
+{
+	char	*new_line;
+	int		len;
+
+	if (!*str)
+		return (NULL);
+	len = 0;
+	while (str[len] != '\0' && str[len] != '\n')
+		len++;
+	new_line = ft_calloc(len + 2);
+	if (str[len] == '\n')
+		new_line[len] = '\n';
+	while (len--)
+		new_line[len] = str[len];
+	return (new_line);
+}
+
+char	*ft_read_buff(int fd, char *str, char *aux, char *buff)
+{
+	int		read_size;
+
+	read_size = 1;
+	while (read_size > 0)
+	{
+		read_size = read(fd, buff, BUFFER_SIZE);
+		if (read_size == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		if (read_size == 0)
+			break ;
+		buff[read_size] = '\0';
+		aux = str;
+		if (aux == NULL)
+			aux = ft_calloc(1);
+		str = ft_strjoin(aux, buff);
+		free(aux);
+		if (ft_strnewline(str))
+			break ;
+	}
+	free(buff);
+	return (str);
+}
+
+char	*ft_get_rest(char *str)
+{
+	int		i;
+	char	*remnant;
+
+	if (!str)
+		return (NULL);
+	if (!ft_strnewline(str))
+	{
+		free(str);
+		return (NULL);
+	}
+	i = 0;
+	while (str[i] != '\n' && str[i])
+		i++;
+	if (str[i] == '\n')
+		i++;
+	remnant = ft_strjoin("", str + i);
+	free(str);
+	return (remnant);
+}
+
+char	*get_next_line(int fd)
+{
+	static char		*str[OPEN_MAX];
+	char			*aux;
+	char			*buff;
+
+	if (fd < 0 || fd > OPEN_MAX)
+		return (NULL);
+	aux = NULL;
+	buff = ft_calloc(BUFFER_SIZE + 1);
+	if (!buff)
+		return (NULL);
+	str[fd] = ft_read_buff(fd, str[fd], aux, buff);
+	if (!str[fd] || !*str[fd])
+		return (NULL);
+	aux = ft_get_line(str[fd]);
+	str[fd] = ft_get_rest(str[fd]);
+	if (!aux)
+	{
+		free(str[fd]);
+		free(aux);
+		return (NULL);
+	}
+	return (aux);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msander- <msander-@student.42sp.org.br     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/07 19:03:34 by msander-          #+#    #+#             */
+/*   Updated: 2022/06/11 15:25:44 by msander-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *c)
+{
+	size_t	lengh;
+
+	lengh = 0;
+	while (*c)
+	{
+		lengh++;
+		c++;
+	}
+	return (lengh);
+}
+
+char	*ft_strjoin(char *str1, char *str2)
+{
+	char	*result;
+	int		len;
+
+	len = ft_strlen(str1) + ft_strlen(str2);
+	result = (char *)malloc(len + 1);
+	if (result == NULL)
+		return (NULL);
+	while (*str1 != '\0')
+	{
+		*result = *str1;
+		result++;
+		str1++;
+	}
+	while (*str2 != '\0')
+	{
+		*result = *str2;
+		result++;
+		str2++;
+	}
+	*result = '\0';
+	return (result - len);
+}
+
+char	*ft_strnewline(const char *s)
+{
+	while (*s != '\0')
+	{
+		if (*s == '\n')
+			return ((char *)s);
+		++s;
+	}
+	return (NULL);
+}
+
+char	*ft_calloc(int size)
+{
+	char	*result;
+
+	result = (char *)malloc(size);
+	if (!result)
+		return (NULL);
+	while (--size >= 0)
+		result[size] = '\0';
+	return (result);
+}
