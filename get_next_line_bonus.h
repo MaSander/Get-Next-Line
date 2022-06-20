a867325edcc794ad4d163e40bd159a90296358b7
MaSander <ma.sander11@gmail.com>
2022-06-20 20:56:24 -0300

    fix bonus header

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*   By: msander- <msander-@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 15:02:07 by msander-          #+#    #+#             */
-/*   Updated: 2022/06/17 18:53:31 by coder            ###   ########.fr       */
+/*   Updated: 2022/06/20 20:55:29 by msander-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # include <unistd.h>
 # include <stdlib.h>
 # define OPEN_MAX 1024
